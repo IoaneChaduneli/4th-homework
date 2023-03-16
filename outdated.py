91419da8251cf0d7c7fc17c4a03f77144ebd2352
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-16 23:39:12 +0400

    changes

--- outdated.py
+++ outdated.py
@@ -1,13 +1,50 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
 
 while True:
-    try:
-        #Enter input the date
-        date = input("Enter date: (MM/DD/YYYY)")
-        #make a list according the separation marks
-        date_1 = date.split("/")
-        #while my list always be including 2 index year formatted by YYYY/MM/DD
-        new_date = f'{date_1[2]}-{date_1[1]:0>2}-{date_1[0]:0>2}'
-    except IndexError:
-        pass
-    else:
-        print(new_date)
+    date = input("MM/DD/YYYY: ")
+
+    if len(date.split("/")) == 3:
+        # convert list and split the chars
+        date = date.split("/")
+
+        print(f'{date[2]}-{date[1]:0>2}-{date[0]:0>2}')
+    
+    # if string and int is in input
+    elif date.replace(" ", "").isalnum():
+
+        for month in months:
+            #understand if month year and day is inputted
+            if len(date.split(" ")) == 3:
+                # convert list 
+                new_date =  date.split(" ")
+                # if input month is written in list
+                if month in date:
+                    # remove string month
+                    new_date.remove(month)
+                    # string month convert to num month
+                    new_date.append(str(months.index(month)+1))
+                    #understand which is more than two charachter
+                    for i in new_date:
+                        if len(i) > 2:
+                            year = i
+                    
+                    print(f'{year}-{new_date[2]:0>2},{new_date[1]:0>2}')
+                    
+                        
+                    
+    
