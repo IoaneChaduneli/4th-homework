146adf2cbc983d362c078957cc35546766d654d3
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-16 16:21:17 +0400

    create grossery list

---
+++ grocery.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+#empty list where we must add elements when the user write
+grossery_list = []
+
+while True:
+    try:
+        #with input create the list of grossery
+        product = input("Grossery List: ").strip().upper()
+        #add product in grossery list
+        grossery_list.append(product)
+        #if end the program terminated its work and read the below codes
+    except KeyboardInterrupt:
+        break
+
+#counter helps to check the frequency and it convert list to dict
+grocery_dict = Counter(grossery_list)
+
+# sorted according to alphabet and take the keys
+product_shown = sorted(grocery_dict.keys())
+
+# in product show the keys are stred in list now i want to take it from the list by loop
+for prod in product_shown:
+    print(f'{grocery_dict[prod]} {prod}')
