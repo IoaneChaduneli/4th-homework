34ed07931f23880f5aeca64862bc8f68413d444c
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-16 21:34:15 +0400

    s

--- outdated.py
+++ outdated.py
@@ -6,7 +6,7 @@ while True:
         #make a list according the separation marks
         date_1 = date.split("/")
         #while my list always be including 2 index year formatted by YYYY/MM/DD
-        new_date = f'{date_1[2]}/{date_1[1]:0>2}/{date_1[0]:0>2}'
+        new_date = f'{date_1[2]}-{date_1[1]:0>2}-{date_1[0]:0>2}'
     except IndexError:
         pass
     else:
