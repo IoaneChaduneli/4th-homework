b15fdbb78baa47629957a996419de1f084b27f6f
Ioane <109984297+IoaneChaduneli@users.noreply.github.com>
2023-03-19 01:11:24 +0400

    how tomake output in appropriate font

---
+++ figlet.py
@@ -0,0 +1,22 @@
+import pyfiglet, sys
+
+figlet = pyfiglet.Figlet()
+
+fonts = figlet.getFonts()
+
+if len(sys.argv) > 2 and sys.argv[1] == '-f' or sys.argv[1] == '--font':
+    font_name = str(sys.argv[2])
+    if font_name in fonts:
+        figlet.setFont(font = font_name)
+    else:
+        print(f"Error: font '{font_name}' not found.")
+    
+else:
+    print(f"Error: font {font_name} not found.")
+    sys.exit()
+
+text = input('text: ')
+
+result = figlet.renderText(text)
+
+print(result)
